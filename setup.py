a55f28550ae8c550989750cbefc3defc2e99130e
Nicola Piga <nicolapiga@gmail.com>
2022-11-05 13:42:12 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="htl",
+    version="0.0",
+    description="",
+    url="",
+    author="",
+    author_email="",
+    license="",
+    packages=find_packages(),
+    install_requires=[
+        "mock",
+        "overrides",
+    ],
+)
