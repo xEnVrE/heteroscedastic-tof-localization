7a156b8bf8993b6291223907721ef636d0f090a1
Nicola Piga <nicolapiga@gmail.com>
2022-11-06 09:27:23 +0000

    Implement torchfilter-free measurement model

--- filter/measurement_model.py
+++ filter/measurement_model.py
@@ -11,23 +11,20 @@ import math
 import numpy
 import torch
 import torch.nn as nn
-import torchfilter
 from fannypack.nn import resblocks
 
 
-class MeasurementModel(torchfilter.base.KalmanFilterMeasurementModel):
+class MeasurementModel(nn.Module):
 
-    def __init__(self, initial_r):
+    def __init__(self, initial_r, batch_size, device):
+        super().__init__()
 
         self.state_dim = 4
         self.measurement_dim = 4
-        # self.measurement_dim = 2
-        # self.inner_measurement_dim = 4
-
-        super().__init__(state_dim = self.state_dim, observation_dim = self.measurement_dim)
+        self.eps = torch.Tensor([.001] * self.measurement_dim).to(device)
+        self.batch_size = batch_size
 
         # Trainable parameters
-        # self.R = torch.nn.Parameter(torch.cholesky(torch.diag(torch.FloatTensor([initial_r for i in range(self.measurement_dim)]))), requires_grad = True)
         units = 64
         self.state_feature = nn.Sequential(nn.Linear(self.state_dim, units), nn.ReLU(), resblocks.Linear(units, activation = 'relu'))
         self.R = nn.Sequential\
@@ -39,41 +36,55 @@ class MeasurementModel(torchfilter.base.KalmanFilterMeasurementModel):
             nn.Linear(units, self.measurement_dim)
         )
 
+        self.sensor_0 = torch.Tensor([0.0, 0.0]).to(device)
+        self.sensor_1 = torch.Tensor([0.0, 2.0]).to(device)
+        self.sensor_2 = torch.Tensor([2.0, 0.0]).to(device)
+        self.sensor_3 = torch.Tensor([2.0, 2.0]).to(device)
+        self.zeros. = torch.zeros(self.measurement_dim).to(device)
+
 
     def forward(self, *, states):
         """Measurement prediction step."""
 
-        N, state_dim = states.shape[:2]
+        B, state_dim = states.shape[:2]
         assert state_dim == self.state_dim
 
-        # Sensors positions (batched)
-        sensor_0 = torch.Tensor([0.0, 0.0])
-        sensor_0 = sensor_0[None, :].repeat(N, 1)
-
-        sensor_1 = torch.Tensor([0.0, 2.0])
-        sensor_1 = sensor_1[None, :].repeat(N, 1)
-
-        sensor_2 = torch.Tensor([2.0, 0.0])
-        sensor_2 = sensor_2[None, :].repeat(N, 1)
-
-        sensor_3 = torch.Tensor([2.0, 2.0])
-        sensor_3 = sensor_3[None, :].repeat(N, 1)
-
         # Distance from predicted state to sensors
-        # d_i = norm(state[0 : 2] - sensor_i)
-        measurements = torch.zeros(N, self.measurement_dim).to(device = states.device)
-        # measurements = torch.zeros(N, self.inner_measurement_dim).to(device = states.device)
+        measurements = self.zeros[None, :].repeat(B, 1)
         position = states[:, 0 : 2]
+
+        # Sensors positions (batched)
+        sensor_0 = self.sensor_0[None, :].repeat(B, 1)
+        sensor_1 = self.sensor_1[None, :].repeat(B, 1)
+        sensor_2 = self.sensor_2[None, :].repeat(B, 1)
+        sensor_3 = self.sensor_3[None, :].repeat(B, 1)
+
         measurements[:, 0] = torch.linalg.norm(position - sensor_0, dim = 1)
         measurements[:, 1] = torch.linalg.norm(position - sensor_1, dim = 1)
         measurements[:, 2] = torch.linalg.norm(position - sensor_2, dim = 1)
         measurements[:, 3] = torch.linalg.norm(position - sensor_3, dim = 1)
-        # measurements = self.predicted_feature(states / 2.0)
 
-        Rs = torch.diag_embed(torch.exp(self.R(self.state_feature(states / 2.0))) + torch.Tensor([.001] * self.measurement_dim))
-        # Rs = torch.exp(self.R(self.state_feature(states / 2.0)).reshape(N, self.measurement_dim, self.measurement_dim))
-        # Rs = self.R[None, :, :].expand(N, self.measurement_dim, self.measurement_dim)
-        test = Rs @ Rs.transpose(-1, -2)
-        test_chol = torch.linalg.cholesky(test)
+        Rs = torch.diag_embed(torch.exp(self.R(self.state_feature(states / 2.0))) + self.eps)
 
         return measurements, Rs
+
+
+    def jacobian(self, states):
+        """Partial derivative of model w.r.t. the state evaluated in states."""
+
+        measurement_dim = self.measurement_dim
+
+        with torch.enable_grad():
+            x = states.detach().clone()
+
+            B, state_dim = x.shape
+            assert state_dim == self.state_dim
+
+            x = x[:, None, :].expand((B, measurement_dim, state_dim))
+            x.requires_grad_(True)
+
+            y = self(states = x.reshape((-1, state_dim)))[0].reshape((B, -1, measurement_dim))
+            mask = torch.eye(measurement_dim, device=x.device).repeat(B, 1, 1)
+            jac = torch.autograd.grad(y, x, mask, create_graph=True)
+
+        return jac[0]
